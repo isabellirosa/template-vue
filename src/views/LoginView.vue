89698963cf63ff8f561df8ed096b387976fdd0dc
isabellirosa <isabelli.ifc@gmail.com>
2025-06-24 20:08:32 -0300

    feat: Adicionando o passage

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -6,7 +6,7 @@ import "@passageidentity/passage-elements/passage-auth";
   <div class="container">
     <h1>Login</h1>
     <div class="authContainer">
-      <passage-auth app-id="GKj1efF61W4xT1JFyDuKPfsK"></passage-auth>
+      <passage-auth app-id="ZYAMw5hrpNJqgQh9MIyw3S05"></passage-auth>
     </div>
   </div>
 </template>
